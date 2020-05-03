c3877b00d22c4fa3ee61771146807ee095cd87b0
kuldip bajwa <kuldipbajwa@kuldips-MacBook-Pro.local>
2020-05-03 19:28:42 +0100

    updated dependencies

--- src/main/java/com/raptor/apis/students/execute/StudentApplication.java
+++ src/main/java/com/raptor/apis/students/execute/StudentApplication.java
@@ -4,16 +4,18 @@ package com.raptor.apis.students.execute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@ComponentScan("com")
 @EnableJpaRepositories("com.raptor.apis.students.domain.dao")
 @EntityScan("com.raptor.apis.students.domain.dao.entity")
 public class StudentApplication {
 
     public static void main (String ...a) {
-        SpringApplication.run(StudentApplication.class);
+       SpringApplication.run(StudentApplication.class);
     }
 
 }
--- src/main/java/com/raptor/apis/students/expose/StudentController.java
+++ src/main/java/com/raptor/apis/students/expose/StudentController.java
@@ -1,20 +1,31 @@
 package com.raptor.apis.students.expose;
 
 
+import com.raptor.apis.students.domain.dao.entity.Student;
+import com.raptor.apis.students.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/school")
+@RequestMapping("/school")
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService;
 
     @GetMapping(path = "/students" , produces = MediaType.APPLICATION_JSON_VALUE )
-    public ResponseEntity getStudents() {
-        return null;
+    public ResponseEntity<List<Student>> getStudents() {
+        final ResponseEntity<List<Student>> responseEntity = new
+                ResponseEntity<>(studentService.getAllStudents() , HttpStatus.OK);
+
+        return responseEntity;
     }
 
 }
