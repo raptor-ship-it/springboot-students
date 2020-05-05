311bdeb8ae84e515befaa0cd1181c084e8964fbb
kuldip bajwa <kuldipbajwa@kuldips-MacBook-Pro.local>
2020-05-05 21:33:29 +0100

    adding markdown@

--- src/main/java/com/raptor/apis/students/execute/StudentApplication.java
+++ src/main/java/com/raptor/apis/students/execute/StudentApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @ComponentScan("com")
 @EnableJpaRepositories("com.raptor.apis.students.domain.dao")
 @EntityScan("com.raptor.apis.students.domain.dao.entity")
+@EnableSwagger2
 public class StudentApplication {
 
     public static void main (String ...a) {
